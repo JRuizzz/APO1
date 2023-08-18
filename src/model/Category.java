635c943dd8fcd62bd20b7135dd299dbbeaa18a35
Juan Esteban Ruiz Ome <juanesteban.ruizome@gmail.com>
2023-08-17 23:02:48 -0500

    Nuevo funcionamiento

--- src/model/Category.java
+++
@@ -1,5 +0,0 @@
-package model;
-
-public enum Category {
-    SILVER, GOLD, DIAMOND;
-}
--- src/model/Regular.java
+++
@@ -1,9 +0,0 @@
-package model;
-import java.util.Calendar;
-public class Regular extends User {
-
-    public Regular(String id, String name, String nickname, Calendar signUpDate) {
-        super(id, name, nickname, signUpDate);
-    }
-    
-}
