fb4557b1366d988e87a49c0fe7498c5bd7b98f08
Juan Esteban Ruiz Ome <juanesteban.ruizome@gmail.com>
2023-08-17 22:22:47 -0500

    APO I

---
+++ src/model/Regular.java
@@ -0,0 +1,9 @@
+package model;
+import java.util.Calendar;
+public class Regular extends User {
+
+    public Regular(String id, String name, String nickname, Calendar signUpDate) {
+        super(id, name, nickname, signUpDate);
+    }
+    
+}
